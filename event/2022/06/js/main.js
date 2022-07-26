d6907b279ed0ab19a139db0d2c137a26fd168c61
shoushangdetuolaji <tklijunhao@qq.com>
2022-07-26 18:03:09 +0900

    feat: 6月份登录奖励活动页轮播图初次构建

--- event/2022/06/js/main.js
+++ event/2022/06/js/main.js
@@ -1,93 +1,63 @@
-initHeight();
+// 일일미션
+// daily mission sign if user logined
+// 动态绑定元素的点击事件
+// removeClass() hide() 如果没有规定参数，则该方法将从被选元素中删除所有类
+$(document).on('click', '.tab-list li:not(.comingsoon) a', function(e) {
+	e.preventDefault();
+	var index = $(this).parents('li').index();
+	tabView(index);
+});
+function tabView(idx) {
+	$('.tab-list li').removeClass('on').eq(idx).addClass('on');
+	$('.tab-cont').hide().eq(idx).show();
+}
 
-//gnb 메뉴별 활성화 제어
-$('.gnb > li').on('mouseover', function(e) {
-  $('.lnb-area').stop().hide();
-  $(this).find('.lnb-area').stop().show();
+// 스페셜미션 tab
+// Special mission sign if user logined
+$(document).on('click', '.tab-list2 li:not(.comingsoon) a', function(e) {
+	e.preventDefault();
+	var index = $(this).parents('li').index();
+	tabSpecia(index);
 });
-$('.gnb').on('mouseleave', function(e) {
-  $('.lnb-area').stop().hide();
-  $('.gnb > li.current').find('.lnb-area').stop().show();
-});
-//gnb 메뉴에 따른 페이지별 클래스 부여
-$('.gnb > li.current').each(function() {
-  var currentMenu = $(this).index();
-  var className = '';
-  switch(currentMenu) {
-    case 0: className = 'lobby'
-      break;
-    case 1: className = 'information'
-      break;
-    case 2 : className = 'office'
-      break;
-    case 3 : className = 'shop'
-      break;
-    case 4 : className = 'promotion'
-      break;
-    case 5 : className = 'director'
-      break;
-  }
-  $('#evt_wrap').addClass(className);
-});
+function tabSpecia(idx) {
+	$('.tab-list2 li').removeClass('on').eq(idx).addClass('on');
+	$('.tab-cont-s').hide().eq(idx).show();
+}
 
 
-var gnbTop;
+//가이드 보기 스와이프
 window.onload = function() {
-	gnbTop = $(".gnb-wrap").offset().top;
-	$(this).scrollTop() > gnbTop ? $('body').addClass('scroll') : $('body').removeClass('scroll')
-	
-	$(window).scroll(function(){		
-		$(this).scrollTop() > gnbTop ? $('body').addClass('scroll') : $('body').removeClass('scroll')
-		gnbResize();
+	// //slide
+	var mySwiper = new Swiper('.mySwiper', {
+		updateOnWindowResize: true,
+		touchRatio: 0,
+		navigation: {
+			nextEl: '.swiper-next-button',
+			prevEl: '.swiper-prev-button',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'fraction',
+			clickable: true,
+		},
+		watchOverflow: true,
+		on: {
+			slideChange: function () {
+				//개발 호출 콜백 함수(페이징 콜백)
+				SetPageNumber(this.realIndex);
+			}
+		},
 	});
-
-	$(window).resize(function() {
-		gnbResize();
-	});
-	gnbResize();
 };
 
-function gnbResize() {
-	//gnb fixed 여부에 따른 스타일 분기
-	var isScroll = $('body').hasClass('scroll') ? -$(window).scrollLeft() : 0
-	$('.gnb-wrap').css({
-		left: isScroll,
-		width: $('#evt_wrap').width()
-	});
-}
+function SetPageNumber(index) {
+	var page;
+	page = index + 1;
 
-//gnb 높이 가변에 따른 event min-height 100% init
-function initHeight() {
-	$('#evt_wrap').css({
-		'min-height': 'calc(100% - ' + $('#evt_wrap').offset().top + 'px)'
-	});
-}
+	if(page == 3)
+	{
+			$('span[data-type="title"]');
 
-
-var isAnagram = function (s, t) {
-  if (s.length !== t.length) {
-    return false;
-  }
-  const map = new Map();
-  for (let i = 0; i<s.length;i++) {
-    if (map.has[s[i]]) {
-      map.set(s[i], map.get(s[i])+1);
-    } else {
-      map.set(s[i], 1)
-    }
-    if (map.has[t[i]]) {
-      map.set(t[i], map.get(t[i])-1);
-    } else {
-      map.set(t[i], -1)
-    }
-  }
-  for (const letter of map) {
-    console.log(letter)
-    console.log(map)
-    if (letter[1]!==0) {
-      return false;
-    }
-  }
-  return true;
+			$($(".popup" + this.$popup.selector + " .btn-guide-reward")[0]).attr("disabled", false);
+	}
 }
-isAnagram('anagram', 'nagaram')
