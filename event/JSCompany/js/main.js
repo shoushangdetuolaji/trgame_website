54ca15371a5c5185c04d99c7b9375fb564292a4b
shoushangdetuolaji <tklijunhao@qq.com>
2022-08-16 17:52:41 +0900

    feat: 完成广告宣传页面&谭燕部长介绍页

--- event/JSCompany/js/main.js
+++ event/JSCompany/js/main.js
@@ -1,4 +1,61 @@
+// promotion swiperData
+
+var swiperData = {
+  1: {
+    "tit": "§ 마고 & 염라의 17주년 축사 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/CUJJZ9j3Wqk?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  2: {
+    "tit": "§ 저승컴퍼니 웹툰 3화 §",
+    "url": "<div class=\"image\"><img src=\"../Promotion/images/img_slide1_1.jpg\" alt=\"웹툰 3-1\"><img src=\"../Promotion/images/img_slide1_2.jpg\" alt=\"웹툰 3-2\"><img src=\"../Promotion/images/img_slide1_3.jpg\" alt=\"웹툰 3-3\"><img src=\"../Promotion/images/img_slide1_4.jpg\" alt=\"웹툰 3-4\"><img src=\"../Promotion/images/img_slide1_5.jpg\" alt=\"웹툰 3-5\"><img src=\"../Promotion/images/img_slide1_6.jpg\" alt=\"웹툰 3-6\"></div>",
+  },
+  3: {
+    "tit": "§ 저승컴퍼니 웹툰 2화 §",
+    "url": "<div class=\"image\"><img src=\"../Promotion/images/img_slide2_1.jpg\" alt=\"웹툰 2-1\"><img src=\"../Promotion/images/img_slide2_2.jpg\" alt=\"웹툰 2-2\"><img src=\"../Promotion/images/img_slide2_3.jpg\" alt=\"웹툰 2-3\"><img src=\"../Promotion/images/img_slide2_4.jpg\" alt=\"웹툰 2-4\"></div>",
+  },
+  4: {
+    "tit": "§ 신입 차사 오리엔테이션 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/oxW_SrDa1pE?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  5: {
+    "tit": "§ 담연 부장 소개 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/BhtxYEGYj7E?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  6: {
+    "tit": "§ 저승 전경 §",
+    "url": "<div class=\"image\"><img src=\"../Promotion/images/img_slide5.jpg\" alt=\"저승 전경 이미지\" width=\"100%\" height=\"100%\"></div>",
+  },
+  7: {
+    "tit": "§ 저승컴퍼니 : 프롤로그 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/nv_5FaVDJaw?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  8: {
+    "tit": "§ 신입 차사 공개 채용 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/vYcXfgxKaM0?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  9: {
+    "tit": "§ 저승컴퍼니 기업 광고 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/wHbrQOMWxik?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  10: {
+    "tit": "§ 저승컴퍼니 웹툰 1화 §",
+    "url": "<div class=\"image\"><img src=\"../Promotion/images/img_slide9_1.jpg\" alt=\"웹툰 1-1\"><img src=\"../Promotion/images/img_slide9_2.jpg\" alt=\"웹툰 1-2\"><img src=\"../Promotion/images/img_slide9_3.jpg\" alt=\"웹툰 1-3\"><img src=\"../Promotion/images/img_slide9_4.jpg\" alt=\"웹툰 1-4\"><img src=\"../Promotion/images/img_slide9_5.jpg\" alt=\"웹툰 1-5\"></div>",
+  },
+  11: {
+    "tit": "§ 업데이트 트레일러 §",
+    "url": "<div class=\"youtube\"><iframe src=\"https://www.youtube.com/embed/PsURbJZFQgs?enablejsapi=1&rel=0\" frameborder=\"0\" allowfullscreen></iframe></div>",
+  },
+  12: {
+    "tit": "§ 저승컴퍼니 건물 §",
+    "url": "<div class=\"image\"><img src=\"../Promotion/images/img_slide11.jpg\" alt=\"저승컴퍼니 건물 이미지\" width=\"100%\" height=\"100%\"></div>",
+  },
+}
+
+
+
 initHeight();
+setSwiper();
+
 // 当页面加载完元素
 if ($('.main').length > 0) {
   // TimelineMax 来自librarys的动画工具函数
@@ -103,3 +160,123 @@ function initHeight() {
 //   return true;
 // }
 // isAnagram('anagram', 'nagaram')
+
+
+// Promotion板块的swiper 定义
+var swiper;
+function setSwiper() {
+	swiper = new Swiper('#swiper', {
+		slidesPerView: 1,
+		loop: true,
+		speed : 0,
+		touchRatio: 0,	//드래그 금지
+		pagination: {
+			el: '.swiper-pagination',
+			type: "fraction",
+		},
+		navigation: {
+			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next',
+		},
+		on: {
+			init: function() {
+				// console.log( this.realIndex );
+			},
+			slideChange: function() {
+				insertSwiperData( this.realIndex +1 );
+				stopSwiperYoutube();
+			}
+		}
+	});
+}
+function insertSwiperData(index) {
+	var title = swiperData[index]["tit"];
+	$('.popup-tit').html(title);
+}
+function stopSwiperYoutube() {
+	$('.swiper-slide').each(function () {
+		var youtubePlayer = $(this).find('iframe').get(0);
+		if (youtubePlayer) {
+			//iframe src must contain "?enablejsapi=1" It will allow to use youtube api to pause video on slide change
+			youtubePlayer.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+		}
+	});
+}
+
+//회사홍보관
+var isClick = 0;
+$(document).on('click', '.promotion-area li button', function() {
+	var idx = $(this).parents('li').attr('data-slide-num');
+	popup('[data-popup-id=promotion]');
+
+	$('#swiper .swiper-slide').each(function(e) {
+		if ( isClick == 0 ) {
+			var dataNum = $(this).attr('data-slide-num');
+			var data = swiperData[dataNum]["url"];
+			$(this).html(data);
+		}			
+	});
+	insertSwiperData(idx);
+	isClick = 1;
+	swiper.slideTo(idx);
+
+	//개발 호출 함수
+	// fnCallGA(idx);
+});
+
+
+//Popup
+var $popup;
+function popup(obj, msgNum, url) {
+	$popup = $(obj);
+	$popup.attr('tabindex', 0).addClass('show').focus();
+	$popup.blur(function() {
+		$(this).removeAttr('tabindex');
+	});
+	var targetY = $(window).scrollTop()+100;
+		h = $popup.outerHeight()/2, w = $popup.outerWidth()/2;
+		
+	//알럿&컨펌 팝업의 경우 모달형식으로 띄움
+	if ( $popup.attr('data-popup-type') == 'modal' ) {
+		$('body').addClass('modal');
+		$popup.find('.popup-msg').html(msgNum);
+		$popup.addClass("show");
+	}
+	else {
+		$('body').addClass('dimmed');
+		$popup.addClass("show").css({top:targetY,opacity:1});
+	}
+
+	if ( $popup.attr('data-popup-type') == 'youtube') {
+		$popup.find('.iframe').empty().append('<iframe src="https://www.youtube.com/embed/'+url+'?autoplay=0&rel=0&amp;controls=1&amp;showinfo=0&amp;color=white" allow="autoplay; encrypted-media" width="100%" height="100%" allowfullscreen></iframe>');
+	}
+	return false;
+}
+
+
+// popupClose
+function popupClose(that) {
+	$("body").removeClass("dimmed").unbind('touchmove');
+	var type = typeof(that); //this == "object"
+	if ( type == "object" ) {
+		$(that).parents('.popup, .talk-modal').removeAttr("style").removeClass("show");
+		$(that).parents('.popup, .talk-modal').find(".iframe iframe").remove();
+		if ($(that).parents('.popup, .talk-modal').attr('data-popup-type') == 'modal') {
+			$('body').removeClass('modal');
+		}
+		else {
+			$('body').removeClass('dimmed');
+		}
+
+		// if ( $(that).parents('.popup, .talk-modal').attr('data-popup-type') == 'item') {
+		// 	if ( $(that).parents('.popup, .talk-modal').attr('data-popup-re') != 're') {
+		// 		document.location.reload();
+		// 	}
+		// }
+		if ( $popup.attr('data-popup-id') == 'promotion') {
+			stopSwiperYoutube();
+		}
+	} else {
+		$(that).parents('.popup').removeAttr("style").removeClass("show");
+	}
+}
